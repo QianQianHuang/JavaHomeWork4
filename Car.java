bff6b15d1c5e5ae74850512a55d667ff720f30f2
QainQainHuang <edison90829@gmail.com>
2020-04-02 00:20:25 +0800

    Car

--- Car.java
+++ Car.java
@@ -45,37 +45,37 @@ import java.util.* ;
      String newColor = scn.next() ;
      this.color = newColor ;
 
-    // ----capacicy----
-    System.out.println() ;
-    System.out.println("Set Capacicy : ") ;
-    String newCapacicy = scn.next() ;
-    this.capacicy = newCapacicy ;
-    
-    // ----powertype----
-    System.out.println() ;
-    System.out.println("Set PowerType : ") ;
-    String newPowerType = scn.next() ;
-    this.powertype = newPowerType ;
-    
-    // ----year----
-    System.out.println() ;
-    System.out.println("Set Year : ") ;
-    String newYear = scn.next() ;
-    this.year = newYear ;
-    
-    // ----weight----
-    System.out.println() ;
-    System.out.println("Set Weight : ") ;
-    int newWeight = scn.nextInt() ;
-    this.weight = newWeight ;
-    
-    // ----displacement----
-    System.out.println() ;
-    System.out.println("Set Displacement : ") ;
-    int newDisplacement = scn.nextInt() ;
-    this.displacement = newDisplacement ;
- }
-
+     // ----capacicy----
+     System.out.println() ;
+     System.out.println("Set Capacicy : ") ;
+     String newCapacicy = scn.next() ;
+     this.capacicy = newCapacicy ;
+     
+     // ----powertype----
+     System.out.println() ;
+     System.out.println("Set PowerType : ") ;
+     String newPowerType = scn.next() ;
+     this.powertype = newPowerType ;
+     
+     // ----year----
+     System.out.println() ;
+     System.out.println("Set Year : ") ;
+     String newYear = scn.next() ;
+     this.year = newYear ;
+     
+     // ----weight----
+     System.out.println() ;
+     System.out.println("Set Weight : ") ;
+     int newWeight = scn.nextInt() ;
+     this.weight = newWeight ;
+     
+     // ----displacement----
+     System.out.println() ;
+     System.out.println("Set Displacement : ") ;
+     int newDisplacement = scn.nextInt() ;
+     this.displacement = newDisplacement ;
+ } 
+ 
  //----get Color----
  public String getColor()
  {
